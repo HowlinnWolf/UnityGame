1674de53642f231b77c45372708be2c5384554c4
HowlinnWolf <howlinnwolf@gmail.com>
2017-05-28 01:24:14 +0300

    v0.9
    - Slightly changed Title Screen
    - GUI added (not all, tho)

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,32 +5,46 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour {
 
 	bool paused = false;
+	public GameObject blurryBack;
+	public GameObject pauseMenu;
+	public GameObject pauseButton;
+
+	private float curVol;
+	private float curTimeScale;
 
 	public void pauseGame(){
 		if (!paused) {
+			curVol = AudioListener.volume;
+			curTimeScale = Time.timeScale;
+			pauseButton.SetActive (false);
+			blurryBack.SetActive (true);
+			pauseMenu.SetActive (true);
 			Time.timeScale = 0f;
-			AudioListener.pause = true;
-			AudioListener.volume = 0f;
+			AudioListener.volume = 0.2f;
 			paused = true;
 		} else {
-			Time.timeScale = 1f;
-			AudioListener.pause = false;
-			AudioListener.volume = 0.8f;
+			blurryBack.SetActive (false);
+			pauseMenu.SetActive (false);
+			Time.timeScale = curTimeScale;
+			AudioListener.volume = curVol;
 			paused = false;
 		}
 	}
 
 	void Update(){
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.Escape)) {	// Pause and Unpause with "back" button
 			pauseGame();
 		}
 	}
 
-	public void Play(){
-		SceneManager.LoadScene("test");
+	public void Play(string scene){
+		SceneManager.LoadScene(scene);
 	}
 
 	public void BackToMenu(){
+		Time.timeScale = 1f;
+		AudioListener.volume = 0.8f;
+		paused = false;
 		SceneManager.LoadScene("MainMenu");
 	}
 
