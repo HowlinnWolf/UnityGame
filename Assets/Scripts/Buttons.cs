f318991c25a026cc86ce0a32fe849dd082a91864
HowlinnWolf <howlinnwolf@gmail.com>
2017-06-01 06:28:48 +0300

    map?
    Заглушка под карту создана.
    Работает хп бар.

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour {
 
 	bool paused = false;
-	//public GameObject darkTexture; // To cover gameplay while you're in the menu and/or not touching screen
 	public GameObject pauseMenu;
 	public GameObject pauseButton;
 	public GameObject fadingImage;
@@ -16,21 +15,27 @@ public class Buttons : MonoBehaviour {
 
 	void Start () {
 		AudioListener.volume = 0.4f;
-		fadeInOut = fadingImage.GetComponent<FadeInOut> ();
+		if (fadeInOut) {
+			fadeInOut = fadingImage.GetComponent<FadeInOut> ();
+		}
 	}
 
 	public void pauseGame(){
 		if (!paused) {
-			fadeInOut.Fade (0.7f, 0.1f);
+			if (fadeInOut) {
+				fadeInOut.Fade (0.7f, 0.1f);
+			}
 			curVol = AudioListener.volume;
 			curTimeScale = Time.timeScale;
 			pauseButton.SetActive (false);
 			pauseMenu.SetActive (true);
 			Time.timeScale = 0f;
-			AudioListener.volume = Mathf.Lerp (AudioListener.volume, 0.2f, 0.2f);
+			AudioListener.volume = Mathf.Lerp (AudioListener.volume, 0.1f, 0.2f);
 			paused = true;
 		} else {
-			fadeInOut.Fade (0.3f, 0.1f);
+			if (fadeInOut) {
+				fadeInOut.Fade (0.3f, 0.1f);
+			}
 			pauseButton.SetActive (true);
 			pauseMenu.SetActive (false);
 			Time.timeScale = curTimeScale;
